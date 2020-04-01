5e507e1c8358a7ea0e369317a38b1296431fd590
d.tanaka <d.tanaka@lincraft.co.jp>
2020-04-01 16:00:28 +0900

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace UDPFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.TextBox3 = new System.Windows.Forms.TextBox();
+            this.TextBox4 = new System.Windows.Forms.TextBox();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.RichTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(99, 61);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(100, 25);
+            this.TextBox1.TabIndex = 0;
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(518, 64);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(100, 25);
+            this.TextBox2.TabIndex = 1;
+            // 
+            // TextBox3
+            // 
+            this.TextBox3.Location = new System.Drawing.Point(710, 64);
+            this.TextBox3.Name = "TextBox3";
+            this.TextBox3.Size = new System.Drawing.Size(100, 25);
+            this.TextBox3.TabIndex = 2;
+            // 
+            // TextBox4
+            // 
+            this.TextBox4.Location = new System.Drawing.Point(36, 136);
+            this.TextBox4.Name = "TextBox4";
+            this.TextBox4.Size = new System.Drawing.Size(668, 25);
+            this.TextBox4.TabIndex = 3;
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(220, 61);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(101, 28);
+            this.Button1.TabIndex = 4;
+            this.Button1.Text = "接続";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(763, 171);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(80, 30);
+            this.Button2.TabIndex = 5;
+            this.Button2.Text = "送信";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // RichTextBox1
+            // 
+            this.RichTextBox1.Location = new System.Drawing.Point(36, 207);
+            this.RichTextBox1.Name = "RichTextBox1";
+            this.RichTextBox1.Size = new System.Drawing.Size(807, 138);
+            this.RichTextBox1.TabIndex = 6;
+            this.RichTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ポート：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(412, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "IPアドレス：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(644, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "ポート：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(404, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(406, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "②送信先のアドレス・ポート、メッセージを入力して送信。";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(273, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "①受信に使うポートを入力して接続。";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 18);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "送信するメッセージ：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 384);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RichTextBox1);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.TextBox4);
+            this.Controls.Add(this.TextBox3);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.TextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.TextBox TextBox2;
+        private System.Windows.Forms.TextBox TextBox3;
+        private System.Windows.Forms.TextBox TextBox4;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button Button2;
+        private System.Windows.Forms.RichTextBox RichTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace UDPFormsApp
+{
+
+    public partial class Form1 : Form
+    {
+        private UdpClient udpClient;
+        private Encoding encoding;
+        private DateTime dateTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //クライアントの設定
+            udpClient = null;
+            //エンコーディングの設定
+            encoding = Encoding.UTF8;
+            //受信可能な状態でないと送信ボタンが押せないよう設定
+            Button2.Enabled = false;
+
+        }
+
+        //Button1のClickイベントハンドラ:データ受信の待機
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //UdpClientを作成
+                udpClient = new UdpClient(int.Parse(TextBox1.Text));
+                //非同期的なデータ受信を開始する
+                udpClient.BeginReceive(ReceiveCallback, udpClient);
+            } 
+            catch (SocketException ex)
+            {
+                Console.WriteLine("受信待機時エラー({0}/{1})",
+                    ex.Message, ex.ErrorCode);
+                return;
+            }
+
+            string displayMsg = string.Format("[you] > データ受信の待機中　ポート：" + TextBox1.Text);
+            RichTextBox1.BeginInvoke(
+                new Action<string>(ShowTextboxString), displayMsg);
+
+            //同じポートで２回以上接続開始しないようにボタンを押せなくする
+            Button1.Enabled = false;
+
+            //送信ボタンが押せるようにする
+            Button2.Enabled = true;
+        }
+
+        //データ受信時の処理
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            UdpClient udp =(System.Net.Sockets.UdpClient)ar.AsyncState;
+
+            //非同期受信を終了する
+            System.Net.IPEndPoint remoteEP = null;
+
+            byte[] rcvBytes;
+
+            try
+            {
+                rcvBytes = udp.EndReceive(ar, ref remoteEP);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("データ受信エラー({0}/{1})",
+                    ex.Message, ex.ErrorCode);
+                return;
+            }
+            catch (ObjectDisposedException ex)
+            {
+                //すでに閉じている時は終了
+                Console.WriteLine("Socketは閉じられています。");
+                return;
+            }
+
+            //データを文字列に変換・テキストボックスに表示
+            string rcvMsg = System.Text.Encoding.UTF8.GetString(rcvBytes);
+
+            string displayMsg = string.Format("[{0} ({1})] > {2}",
+                remoteEP.Address, remoteEP.Port, rcvMsg);
+
+            RichTextBox1.BeginInvoke(
+                new Action<string>(ShowTextboxString), displayMsg);
+
+            //再びデータ受信を開始する
+            udp.BeginReceive(ReceiveCallback, udp);
+        }
+
+        //Button2のClickイベントハンドラ：データを送信
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //送信するデータを作成する
+            byte[] sendBytes = encoding.GetBytes(TextBox4.Text);
+
+            //非同期的にデータを送信する
+            udpClient.BeginSend(sendBytes, sendBytes.Length,
+                TextBox2.Text, int.Parse(TextBox3.Text),
+                SendCallback, udpClient);
+            //自分が送信したメッセージを表示する
+            string displayMsg = string.Format("[you] >" + TextBox4.Text);
+
+            RichTextBox1.BeginInvoke(
+                new Action<string>(ShowTextboxString), displayMsg);
+        }
+
+        //データを送信した時
+        private void SendCallback(IAsyncResult ar)
+        {
+            System.Net.Sockets.UdpClient udp =
+                (System.Net.Sockets.UdpClient)ar.AsyncState;
+
+            //非同期送信を終了する
+            try
+            {
+                udp.EndSend(ar);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("送信エラー({0}/{1})",
+                    ex.Message, ex.ErrorCode);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                //すでに閉じている時は終了
+                Console.WriteLine("Socketは閉じられています。");
+            }
+           
+        }
+
+        //フォームのFormClosedイベントハンドラ：UdpClientを閉じる
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (udpClient != null)
+            {
+                udpClient.Close();
+            }
+        }
+
+        //テキストボックスにメッセージを表示する
+        private void ShowTextboxString(string str)
+        {
+            dateTime = DateTime.Now;
+            RichTextBox1.Text = dateTime + str + "\r\n" + RichTextBox1.Text;
+        }
+    }
+
+}
